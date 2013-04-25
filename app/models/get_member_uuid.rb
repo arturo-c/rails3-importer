07c30d817c168d49dd863563f8af219534305854
arturo-c <arturo.kontreras@gmail.com>
2013-04-25 22:03:01 +0000

    delete

--- app/models/get_member_uuid.rb
+++ app/models/get_member_uuid.rb
@@ -22,13 +22,22 @@ class GetMemberUuid
           if user.parent_email
             parent = client.user_get_email(user.parent_email)
             unless parent == 'No Content'
-              unless Member.where(:email => parent.first['email'], :uuid.exists => true).first
+              m = Member.where(:email => parent.first['email']).first
+              if m
+                unless m.uuid
+                  m.update_attributes(:uuid => parent.first['uuid'])
+                end
+              else
                 p = parent.first
                 Member.create({:admin_uuid => user.admin_uuid, :email => p['email'], :uuid => p['uuid'], :gender => p['gender'], :first_name => p['firstname'], :last_name => p['lastname'], :birthday => Date.strptime(p['birthday'], "%m/%d/%Y").to_s, :status => 'AllPlayers'})
               end
               user.create_child
             else
-              raise 'Parent not found'
+              if Member.where(:email => user.parent_email).first
+                user.create_child
+              else
+                raise 'Parent not found'
+              end
             end
           end
           user.status = 'Ready to be imported.'
