7faf339a7a82a5da41064acc9c34683159e914b4
arturo-c <arturo.kontreras@gmail.com>
2013-05-07 21:31:40 +0000

    delete

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -2,11 +2,11 @@ module MembersHelper
   def export_member_data
     require 'csv'
     CSV.generate do |csv|
-      csv << Member.column_names.merge(['group_uuid'])
+      csv << Member.column_names
       @members.each do |m|
         group = Group.where(:uuid => m.group_uuid).first if m.group_uuid
         group = Group.where(:name => m.group_name).first unless m.group_uuid
-        csv << m.columns.merge([group.uuid])
+        csv << m
       end
     end
   end 
