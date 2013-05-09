05f0d77929e6d6cd80fda30ee106654cbefa9b59
arturo-c <arturo.kontreras@gmail.com>
2013-05-09 22:32:00 +0000

    delete

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -5,7 +5,7 @@ module MembersHelper
       csv << ['_id', 'member_id', 'errors', 'parent_email', 'email', 'first_name',
         'last_name', 'uuid', 'birthday', 'gender', 'group_name', 'roles',
         'address_1', 'address_2', 'city', 'state', 'zip', 'country', 'phone',
-        'submission_id', 'group_uuid']
+        'submission_id', 'group_uuid', 'join_date']
       @members.each do |m|
         group = Group.where(:uuid => m.group_uuid).first if m.group_uuid
         group = Group.where(:name => m.group_name).first unless m.group_uuid
@@ -13,7 +13,7 @@ module MembersHelper
           m.first_name, m.last_name, m.uuid, m.birthday,
           m.gender, m.group_name, m.roles ? m.roles.join(', ') : '',
           m.address_1, m.address_2, m.city, m.state, m.zip, m.country, m.phone,
-          m.submission_id, group ? group.uuid : '']
+          m.submission_id, group ? group.uuid : '', m.join_date ? m.join_date : '']
       end
     end
   end 
