1372faafe0bde23b3afee2a64df423d571a23b27
arturo-c <arturo.kontreras@gmail.com>
2013-09-25 19:02:44 +0000

    delete

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -4,7 +4,7 @@ module GroupsHelper
     CSV.generate do |csv|
       csv << ['uuid', 'name', 'description', 'zip', 'type', 'category', 'groups_above']
       @groups.each do |g|
-        csv << [g.uuid, g.name, g.description, g.location['zip'], g.type, g.category ? g.category[0] : '', g.groups_above ? g.groups_above.join(', ') : '']
+        csv << [g.uuid, g.name, g.description, g.location ? g.location['zip'] : '', g.type, g.category ? g.category[0] : '', g.groups_above ? g.groups_above.join(', ') : '']
       end
     end
   end
