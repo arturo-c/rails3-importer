ffd6bab0874c445f5befc7eade2caaf1abb1722d
arturo-c <arturo.kontreras@gmail.com>
2013-09-26 01:21:14 +0000

    delete

--- app/models/update_group.rb
+++ app/models/update_group.rb
@@ -3,14 +3,13 @@ class UpdateGroup
 
   def self.perform(group_id)
     group = Group.find(group_id)
-    admin = Admin.where(:uuid => group.user_uuid).first
+    admin = Admin.find_by(:uuid => group.user_uuid)
     client = AllPlayers::Client.new(ENV["HOST"])
     client.prepare_access_token(admin.token, admin.secret, ENV["OMNIAUTH_PROVIDER_KEY"], ENV["OMNIAUTH_PROVIDER_SECRET"])
     begin
       params = {}
       params[:title] = group.name if group.name
-      params[:type] = group.type if group.type
-      params[:category] = {0 => group.category} if group.category
+      params[:group_type] = group.type if group.type
       client.group_update(group.uuid, params)
     rescue => e
       group.err = e.to_s
