27e299fad0a1f2f17183d023a54983c5262962c9
art kontreras <arturo.kontreras@gmail.com>
2014-03-28 16:49:34 -0500

    test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
 # config/unicorn.rb
 @env = ENV['RAILS_ENV'] || 'development'
-@dir = '/mnt/apci/usar_importer/current'
 if @env == 'production'
   listen 80, :tcp_nopush => true
-  shared_path = "#{@dir}/shared"
+  shared_path = "/mnt/apci/usar_importer/shared"
   stderr_path "#{shared_path}/log/unicorn.stderr.log"
   stdout_path "#{shared_path}/log/unicorn.stdout.log"
+  working_directory "/mnt/apci/usar_importer/current"
 else
   listen "#{@dir}/tmp/unicorn.sock", :backlog => 64
 end
