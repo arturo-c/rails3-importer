02db4e39a823b36b8279fc961d89b7c825c2db36
art kontreras <arturo.kontreras@gmail.com>
2014-04-03 10:36:52 -0500

    Fix member export for nil objects on data.

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -18,8 +18,8 @@ module MembersHelper
         data_fields = Array.new
         data = m.data_fields
         @admin.webform_fields.each do |key, name|
-          data_fields << data[key] unless data[key].nil?
-          data_fields << '' if data[key].nil?
+          data_fields << data[key] unless (data.nil? || data[key].nil?)
+          data_fields << '' if (data.nil? || data[key].nil?)
         end
         csv << data_fields.concat([m.member_id, m.first_name, m.last_name, m.title, roles.join(', '), flags.join(', '), m.join_date, m.birthday,m.parent_email, m.email, m.gender, m.uuid, m.group_uuid, m.submission_id, m.err])
       end
