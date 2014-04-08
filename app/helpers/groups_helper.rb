dbdc09e37f7978ed27472c1e16fd60b9be7a514a
art kontreras <arturo.kontreras@gmail.com>
2014-04-08 15:32:07 -0500

    Rearrange order of groups export.

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,9 +2,9 @@ module GroupsHelper
   def export_group_data
     require 'csv'
     CSV.generate do |csv|
-      csv << ['status', 'errors', 'uuid', 'title', 'description', 'address_street', 'address_city', 'address_state', 'address_zip', 'group_type', 'category', 'group_above', 'group_template', 'payee_uuid', 'user_uuid', 'due date', 'user email', 'group above name']
+      csv << ['due date', 'user_uuid', 'user email', 'title', 'uuid', 'group_type', 'category', 'description', 'address_street', 'address_city', 'address_state', 'address_zip', 'group above name', 'group_above', 'group_template', 'payee_uuid', 'status', 'errors']
       @groups.each do |g|
-        csv << [g.status, g.err, g.uuid, g.title, g.description, g.address_street, g.address_city, g.address_state, g.address_zip, g.group_type, g.category, g.group_above, g.group_template, g.payee, g.user_uuid, g.due_date, g.user_email, g.group_above_name]
+        csv << [g.due_date, g.user_uuid, g.user_email, g.title, g.uuid, g.group_type, g.category, g.description, g.address_street, g.address_city, g.address_state, g.address_zip, g.group_above_name, g.group_above, g.group_template, g.payee, g.status, g.err]
       end
     end
   end
