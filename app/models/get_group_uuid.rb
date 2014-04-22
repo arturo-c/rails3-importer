864736aa0d5e6c4cdbef26d52ff5a72bed493f84
art kontreras <arturo.kontreras@gmail.com>
2014-04-22 13:33:45 -0500

    Change get group uuid to search for groups by title.

--- app/models/get_group_uuid.rb
+++ app/models/get_group_uuid.rb
@@ -7,9 +7,9 @@ class GetGroupUuid
     client = AllPlayers::Client.new(ENV["HOST"])
     client.prepare_access_token(admin.token, admin.secret, ENV["OMNIAUTH_PROVIDER_KEY"], ENV["OMNIAUTH_PROVIDER_SECRET"])
     begin
-      g = Group.where(:title => group.title, :status => 'AllPlayers').first
-      group.update_attributes(:uuid => g.uuid)
-      raise unless group.save
+      g = client.group_search({:search => group.title})
+      raise unless g[0]['uuid']
+      group.update_attributes(:uuid => g[0]['uuid'])
     rescue => e
       group.err = e.to_s
       group.status = 'Error getting group'
