493be994a89d44790e7ad1b02c3bf4cbbc5fa963
art kontreras <arturo.kontreras@gmail.com>
2014-09-25 16:15:22 -0500

    Remove old code from update group.

--- app/models/update_group.rb
+++ app/models/update_group.rb
@@ -6,10 +6,7 @@ class UpdateGroup
     client = AllPlayers::Client.new(ENV["HOST"])
     client.add_headers({:Authorization => ActionController::HttpAuthentication::Basic.encode_credentials(ENV["ADMIN_EMAIL"], ENV["ADMIN_PASSWORD"])})
     begin
-      params = {}
-      params[:title] = group.name if group.name
-      params[:group_type] = group.type if group.type
-      client.group_update(group.uuid, params)
+      client.group_update(group.uuid)
     rescue => e
       group.err = e.to_s
       group.status = 'Error updating group'
